e81b4a1eadac3f423bd57b1901f929b4186cf3c3
BartoszWeb <bartoszdadok@gmail.com>
2022-11-05 13:45:26 +0100

    next

--- components/organisms/Offer/Offer.tsx
+++ components/organisms/Offer/Offer.tsx
@@ -20,28 +20,11 @@ import {
     HeadLineH3,
     HeadLineH4,
 } from "./Offer.styles";
-import angelo from "../../../public/assets/hotels/angelo.jpg";
-import cevedale from "../../../public/assets/hotels/cevedale.jpg";
-import derbyclub from "../../../public/assets/hotels/derbyclub.jpg";
-import frontdeneige from "../../../public/assets/hotels/frontdeneige.jpg";
-import golf from "../../../public/assets/hotels/golf.jpg";
-import gentianes from "../../../public/assets/hotels/gentianes.jpg";
-import hausanderluck from "../../../public/assets/hotels/hausanderluck.jpg";
-import lagranbecca from "../../../public/assets/hotels/lagranbecca.jpg";
-import laroccasport from "../../../public/assets/hotels/laroccasport.jpg";
-import leschaletsladonis from "../../../public/assets/hotels/leschaletsladonis.jpg";
-import margarethenbad from "../../../public/assets/hotels/margarethenbad.jpg";
-import misurina from "../../../public/assets/hotels/misurina.jpg";
-import pampeago from "../../../public/assets/hotels/pampeago.jpg";
-import prom from "../../../public/assets/hotels/prom.jpg";
-import solaris from "../../../public/assets/hotels/solaris.jpg";
 import topresidence from "../../../public/assets/hotels/topresidence.jpg";
-import villaggiodolomitico from "../../../public/assets/hotels/villaggiodolomitico.jpg";
+
 
 const images = {
-
     topresidence,
-
 };
 
 const Offer = () => {
