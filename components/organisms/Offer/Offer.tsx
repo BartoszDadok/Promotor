2f520fd5a42a5517f2fae96cabbc3e2b1e8a1379
BartoszWeb <bartoszdadok@gmail.com>
2022-11-05 14:22:34 +0100

    next

--- components/organisms/Offer/Offer.tsx
+++ components/organisms/Offer/Offer.tsx
@@ -28,7 +28,7 @@ import golf from "../../../public/assets/hotels/golf.jpg";
 import gentianes from "../../../public/assets/hotels/gentianes.jpg";
 // import hausanderluck from "../../../public/assets/hotels/hausanderluck.jpg";
 // import lagranbecca from "../../../public/assets/hotels/lagranbecca.jpg";
-import lar from "../../../public/assets/hotels/laroccasport.jpg";
+import lar from "../../../public/assets/hotels/lar.jpg";
 import leschaletsladonis from "../../../public/assets/hotels/leschaletsladonis.jpg";
 import margarethenbad from "../../../public/assets/hotels/margarethenbad.jpg";
 import misurina from "../../../public/assets/hotels/misurina.jpg";
