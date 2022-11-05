6077169ab7aba8b25b1c6dd17ce59601fb6273ec
BartoszWeb <bartoszdadok@gmail.com>
2022-11-05 14:04:16 +0100

    next

--- components/organisms/Offer/Offer.tsx
+++ components/organisms/Offer/Offer.tsx
@@ -22,13 +22,13 @@ import {
 } from "./Offer.styles";
 import angelo from "../../../public/assets/hotels/angelo.jpg";
 import cevedale from "../../../public/assets/hotels/cevedale.jpg";
-import derbyclub from "../../../public/assets/hotels/derbyclub.jpg";
-import frontdeneige from "../../../public/assets/hotels/frontdeneige.jpg";
+// import derbyclub from "../../../public/assets/hotels/derbyclub.jpg";
+// import frontdeneige from "../../../public/assets/hotels/frontdeneige.jpg";
 import golf from "../../../public/assets/hotels/golf.jpg";
 import gentianes from "../../../public/assets/hotels/gentianes.jpg";
-import hausanderluck from "../../../public/assets/hotels/hausanderluck.jpg";
-import lagranbecca from "../../../public/assets/hotels/lagranbecca.jpg";
-import laroccasport from "../../../public/assets/hotels/laroccasport.jpg";
+// import hausanderluck from "../../../public/assets/hotels/hausanderluck.jpg";
+// import lagranbecca from "../../../public/assets/hotels/lagranbecca.jpg";
+// import laroccasport from "../../../public/assets/hotels/laroccasport.jpg";
 import leschaletsladonis from "../../../public/assets/hotels/leschaletsladonis.jpg";
 import margarethenbad from "../../../public/assets/hotels/margarethenbad.jpg";
 import misurina from "../../../public/assets/hotels/misurina.jpg";
@@ -41,13 +41,8 @@ import villaggiodolomitico from "../../../public/assets/hotels/villaggiodolomiti
 const images = {
     angelo,
     cevedale,
-    derbyclub,
-    frontdeneige,
     golf,
     gentianes,
-    hausanderluck,
-    lagranbecca,
-    laroccasport,
     leschaletsladonis,
     margarethenbad,
     misurina,
