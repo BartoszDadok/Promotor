cb7e3b92264a8dabc5b366cd31790172869c9f78
BartoszWeb <bartoszdadok@gmail.com>
2022-12-02 20:42:17 +0100

    mobile hotel

--- pages/margarethenbad/28.01.2023-04.02.2023/index.tsx
+++ pages/margarethenbad/28.01.2023-04.02.2023/index.tsx
@@ -19,70 +19,10 @@ import { ListDescribe } from "../../../components/atoms/HotelList/ListDescribe";
 import { Title } from "../../../components/atoms/HotelTitle/Title";
 import { HeadlineH3 } from "../../../components/atoms/HotelHeadLineH3/HotelHeadLineH3";
 import { Link } from "../../../components/atoms/HotelLink/HotelLink";
-
-import marg1 from "../../../public/assets/hotels/margarethenbad/1.jpg";
-import marg2 from "../../../public/assets/hotels/margarethenbad/2.jpg";
-import marg3 from "../../../public/assets/hotels/margarethenbad/3.jpg";
-import marg4 from "../../../public/assets/hotels/margarethenbad/4.jpg";
-import marg5 from "../../../public/assets/hotels/margarethenbad/5.jpg";
-import marg6 from "../../../public/assets/hotels/margarethenbad/6.jpg";
-import marg7 from "../../../public/assets/hotels/margarethenbad/7.jpg";
-import marg8 from "../../../public/assets/hotels/margarethenbad/8.jpg";
-import marg9 from "../../../public/assets/hotels/margarethenbad/9.jpg";
-import marg10 from "../../../public/assets/hotels/margarethenbad/10.jpg";
-import marg11 from "../../../public/assets/hotels/margarethenbad/11.jpg";
-import marg12 from "../../../public/assets/hotels/margarethenbad/12.jpg";
-import marg13 from "../../../public/assets/hotels/margarethenbad/13.jpg";
-import marg14 from "../../../public/assets/hotels/margarethenbad/14.jpg";
-import marg15 from "../../../public/assets/hotels/margarethenbad/15.jpg";
-import marg16 from "../../../public/assets/hotels/margarethenbad/16.jpg";
-import marg17 from "../../../public/assets/hotels/margarethenbad/17.jpg";
-import marg18 from "../../../public/assets/hotels/margarethenbad/18.jpg";
-import marg19 from "../../../public/assets/hotels/margarethenbad/19.jpg";
-import marg20 from "../../../public/assets/hotels/margarethenbad/20.jpg";
-import marg21 from "../../../public/assets/hotels/margarethenbad/21.jpg";
-import marg22 from "../../../public/assets/hotels/margarethenbad/22.jpg";
-import marg23 from "../../../public/assets/hotels/margarethenbad/23.jpg";
-import marg24 from "../../../public/assets/hotels/margarethenbad/24.jpg";
-import marg25 from "../../../public/assets/hotels/margarethenbad/25.jpg";
-import marg26 from "../../../public/assets/hotels/margarethenbad/26.jpg";
-import marg27 from "../../../public/assets/hotels/margarethenbad/27.jpg";
-import marg28 from "../../../public/assets/hotels/margarethenbad/28.jpg";
-import marg29 from "../../../public/assets/hotels/margarethenbad/29.jpg";
 import { TitleMobile } from "../../../components/atoms/HotelTitleMobile/TitleMobile";
 import MobileGallery from "../../../components/organisms/MobileGallery/MobileGallery";
+import { images } from "../../../utils/margaretImages";
 
-const images = [
-    { url: marg1.src, title: "Hotel Margarethenbad z zewnątrz" },
-    { url: marg2.src, title: "Hotel Margarethenbad stok narciarski" },
-    { url: marg3.src, title: "Hotel Margarethenbad wewnątrz" },
-    { url: marg4.src, title: "Hotel Margarethenbad wewnątrz" },
-    { url: marg5.src, title: "Hotel Margarethenbad wewnątrz" },
-    { url: marg6.src, title: "Hotel Margarethenbad wewnątrz" },
-    { url: marg7.src, title: "Hotel Margarethenbad wewnątrz" },
-    { url: marg8.src, title: "Hotel Margarethenbad wewnątrz" },
-    { url: marg9.src, title: "Hotel Margarethenbad wewnątrz" },
-    { url: marg10.src, title: "Hotel Margarethenbad wewnątrz" },
-    { url: marg11.src, title: "Hotel Margarethenbad wewnątrz" },
-    { url: marg12.src, title: "Hotel Margarethenbad wewnątrz" },
-    { url: marg13.src, title: "Hotel Margarethenbad wewnątrz" },
-    { url: marg14.src, title: "Hotel Margarethenbad wewnątrz" },
-    { url: marg15.src, title: "Hotel Margarethenbad wewnątrz" },
-    { url: marg16.src, title: "Hotel Margarethenbad wewnątrz" },
-    { url: marg17.src, title: "Hotel Margarethenbad wewnątrz" },
-    { url: marg18.src, title: "Hotel Margarethenbad wewnątrz" },
-    { url: marg19.src, title: "Hotel Margarethenbad wewnątrz" },
-    { url: marg20.src, title: "Hotel Margarethenbad wewnątrz" },
-    { url: marg21.src, title: "Hotel Margarethenbad wewnątrz" },
-    { url: marg22.src, title: "Hotel Margarethenbad wewnątrz" },
-    { url: marg23.src, title: "Hotel Margarethenbad wewnątrz" },
-    { url: marg24.src, title: "Hotel Margarethenbad wewnątrz" },
-    { url: marg25.src, title: "Hotel Margarethenbad wewnątrz" },
-    { url: marg26.src, title: "Hotel Margarethenbad wewnątrz" },
-    { url: marg27.src, title: "Hotel Margarethenbad wewnątrzz" },
-    { url: marg28.src, title: "Hotel Margarethenbad z zewnątrz" },
-    { url: marg29.src, title: "Hotel Margarethenbad z zewnątrz" },
-];
 
 const Index = () => {
     const date = ["28.01.2023 - 04.02.2023", "27.01.2023 - 05.02.2023"];
--- pages/misurina/11.03.2023-18.03.2023/index.tsx
+++ pages/misurina/11.03.2023-18.03.2023/index.tsx
@@ -1,5 +1,4 @@
 import React, { useState } from "react";
-import styled from "styled-components";
 import Navigation from "../../../components/organisms/Navigation/Navigation";
 import Modal from "../../../components/organisms/Modal/Modal";
 import Footer from "../../../components/organisms/Footer/Footer";
@@ -20,56 +19,9 @@ import { ListDescribe } from "../../../components/atoms/HotelList/ListDescribe";
 import { Title } from "../../../components/atoms/HotelTitle/Title";
 import { HeadlineH3 } from "../../../components/atoms/HotelHeadLineH3/HotelHeadLineH3";
 import { Link } from "../../../components/atoms/HotelLink/HotelLink";
-
-import misurina1 from "../../../public/assets/hotels/misurina/1.jpg";
-import misurina2 from "../../../public/assets/hotels/misurina/2.jpg";
-import misurina3 from "../../../public/assets/hotels/misurina/3.jpg";
-import misurina4 from "../../../public/assets/hotels/misurina/4.jpg";
-import misurina5 from "../../../public/assets/hotels/misurina/5.jpg";
-import misurina6 from "../../../public/assets/hotels/misurina/6.jpg";
-import misurina7 from "../../../public/assets/hotels/misurina/7.jpg";
-import misurina8 from "../../../public/assets/hotels/misurina/8.jpg";
-import misurina9 from "../../../public/assets/hotels/misurina/9.jpg";
-import misurina10 from "../../../public/assets/hotels/misurina/10.jpg";
-import misurina11 from "../../../public/assets/hotels/misurina/11.jpg";
-import misurina12 from "../../../public/assets/hotels/misurina/12.jpg";
-import misurina13 from "../../../public/assets/hotels/misurina/13.jpg";
-import misurina14 from "../../../public/assets/hotels/misurina/14.jpg";
-import misurina15 from "../../../public/assets/hotels/misurina/15.jpg";
-import misurina16 from "../../../public/assets/hotels/misurina/16.jpg";
-import misurina17 from "../../../public/assets/hotels/misurina/17.jpg";
-import misurina18 from "../../../public/assets/hotels/misurina/18.jpg";
-import misurina19 from "../../../public/assets/hotels/misurina/19.jpg";
-import misurina20 from "../../../public/assets/hotels/misurina/20.jpg";
-import misurina21 from "../../../public/assets/hotels/misurina/21.jpg";
-import misurina22 from "../../../public/assets/hotels/misurina/22.jpg";
 import { TitleMobile } from "../../../components/atoms/HotelTitleMobile/TitleMobile";
 import MobileGallery from "../../../components/organisms/MobileGallery/MobileGallery";
-
-const images = [
-    { url: misurina1.src, title: "Hotel Misurina z zewnątrz" },
-    { url: misurina2.src, title: "Hotel Misurina z zewnątrz" },
-    { url: misurina3.src, title: "Hotel Misurina z zewnątrz" },
-    { url: misurina4.src, title: "Hotel Misurina z zewnątrz" },
-    { url: misurina5.src, title: "Hotel Misurina z zewnątrz" },
-    { url: misurina6.src, title: "Hotel Misurina wewnątrz" },
-    { url: misurina7.src, title: "Hotel Misurina wewnątrz" },
-    { url: misurina8.src, title: "Hotel Misurina wewnątrz" },
-    { url: misurina9.src, title: "Hotel Misurina wewnątrz" },
-    { url: misurina10.src, title: "Hotel Misurina wewnątrz" },
-    { url: misurina11.src, title: "Hotel Misurina wewnątrz" },
-    { url: misurina12.src, title: "Hotel Misurina wewnątrz" },
-    { url: misurina13.src, title: "Hotel Misurina wewnątrz" },
-    { url: misurina14.src, title: "Hotel Misurina wewnątrz" },
-    { url: misurina15.src, title: "Hotel Misurina wewnątrz" },
-    { url: misurina16.src, title: "Hotel Misurina wewnątrz" },
-    { url: misurina17.src, title: "Hotel Misurina wewnątrz" },
-    { url: misurina18.src, title: "Hotel Misurina wewnątrz" },
-    { url: misurina19.src, title: "Hotel Misurina wewnątrz" },
-    { url: misurina20.src, title: "Hotel Misurina wewnątrz" },
-    { url: misurina21.src, title: "Hotel Misurina wewnątrz" },
-    { url: misurina22.src, title: "Hotel Misurina z zewnątrz" },
-];
+import { images } from "../../../utils/misurinaImages";
 
 
 const Index = () => {
