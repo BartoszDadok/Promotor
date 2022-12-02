d0fbc152cee7a92795a1df20212d164ac86d79ad
BartoszWeb <bartoszdadok@gmail.com>
2022-12-02 20:48:44 +0100

    mobile hotel

--- utils/gentiantesImages.ts
+++ utils/gentiantesImages.ts
@@ -1,18 +1,18 @@
-import gent1 from "../../../public/assets/hotels/gentianes/1.jpg";
-import gent2 from "../../../public/assets/hotels/gentianes/2.jpg";
-import gent3 from "../../../public/assets/hotels/gentianes/3.jpg";
-import gent4 from "../../../public/assets/hotels/gentianes/4.jpg";
-import gent5 from "../../../public/assets/hotels/gentianes/5.jpg";
-import gent6 from "../../../public/assets/hotels/gentianes/6.jpg";
-import gent7 from "../../../public/assets/hotels/gentianes/7.jpg";
-import gent8 from "../../../public/assets/hotels/gentianes/8.jpg";
-import gent9 from "../../../public/assets/hotels/gentianes/9.jpg";
-import gent10 from "../../../public/assets/hotels/gentianes/10.jpg";
-import gent11 from "../../../public/assets/hotels/gentianes/11.jpg";
-import gent12 from "../../../public/assets/hotels/gentianes/12.jpg";
-import gent13 from "../../../public/assets/hotels/gentianes/13.jpg";
-import gent14 from "../../../public/assets/hotels/gentianes/14.jpg";
-import gent15 from "../../../public/assets/hotels/gentianes/15.jpg";
+import gent1 from "../public/assets/hotels/gentianes/1.jpg";
+import gent2 from "../public/assets/hotels/gentianes/2.jpg";
+import gent3 from "../public/assets/hotels/gentianes/3.jpg";
+import gent4 from "../public/assets/hotels/gentianes/4.jpg";
+import gent5 from "../public/assets/hotels/gentianes/5.jpg";
+import gent6 from "../public/assets/hotels/gentianes/6.jpg";
+import gent7 from "../public/assets/hotels/gentianes/7.jpg";
+import gent8 from "../public/assets/hotels/gentianes/8.jpg";
+import gent9 from "../public/assets/hotels/gentianes/9.jpg";
+import gent10 from "../public/assets/hotels/gentianes/10.jpg";
+import gent11 from "../public/assets/hotels/gentianes/11.jpg";
+import gent12 from "../public/assets/hotels/gentianes/12.jpg";
+import gent13 from "../public/assets/hotels/gentianes/13.jpg";
+import gent14 from "../public/assets/hotels/gentianes/14.jpg";
+import gent15 from "../public/assets/hotels/gentianes/15.jpg";
 
 export const images = [
     { url: gent1.src, title: "Apartament Gentianes z zewnątrz" },
