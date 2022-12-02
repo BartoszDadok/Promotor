a9b61d91d64f38de0639dfe54b54dfd85aaf1c77
BartoszWeb <bartoszdadok@gmail.com>
2022-12-02 17:22:49 +0100

    mobile hotel

--- components/atoms/HeroImage/HeroImage.tsx
+++ components/atoms/HeroImage/HeroImage.tsx
@@ -1,15 +1,21 @@
 import React from "react";
 import heroImgSrc from "../../../public/assets/promotor-family-image.jpg";
 import Image from "next/image";
+import styled from "styled-components";
 
+const Picture = styled.picture`
+  display: block;
+  width: 100%;
+  height: 100%;
+`;
 const HeroImage = () => {
     return (
-        <picture>
+        <Picture>
             <source srcSet={ heroImgSrc.src } media="(min-width: 1000px)"/>
             <Image unoptimized={ true } src={ heroImgSrc } alt="Rodzina ciesząca się zimowymi wakacjami"
                    priority={ true }/>
 
-        </picture>
+        </Picture>
     );
 };
 
